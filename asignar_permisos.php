14a945543b1158f31d5050be8abc0735f882bd9c
WaxXaa <mfalejesus@criptext.com>
2024-10-10 23:24:38 -0500

    sc

--- asignar_permisos.php
+++ asignar_permisos.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Asignar Permisos a Roles</title>
+    <link rel="stylesheet" href="./public/styles.css">
+
 </head>
 <body>
     <h2>Asignar Permisos a Roles</h2>

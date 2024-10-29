5dfca2ca822dd6fcfbce0a4d059f97b61c5bf7ad
WaxXaa <mfalejesus@criptext.com>
2024-10-28 22:00:37 -0500

    actualizando

--- app/core/App.php
+++ app/core/App.php
@@ -1,6 +1,6 @@
 <?php
-include_once '../controllers/usuarioControlador.php'
-include_once '../Controllers/seguridadControlador.php'
+include_once '../controllers/usuarioControlador.php';
+include_once '../Controllers/seguridadControlador.php';
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
   if(isset($_POST['login'])) {
     session_start();
---
+++ registro_permiso.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrar Permiso</title>
+    <link rel="stylesheet" href="./public/styles.css">
+</head>
+<body>
+    <h2>Registrar Permiso</h2>
+    <form action="/registrar_permiso" method="post">
+        <label for="permiso_nombre">Nombre del Permiso:</label>
+        <input type="text" id="permiso_nombre" name="permiso_nombre" required><br><br>
+        <button type="submit">Registrar Permiso</button>
+    </form>
+</body>
+</html>

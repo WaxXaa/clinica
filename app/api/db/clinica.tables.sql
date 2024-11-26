e40c1d700b30f79e0ca9f8d3428e1227621a0b1d
WaxXaa <mfalejesus@criptext.com>
2024-11-25 23:13:39 -0500

    reorganizando departamentos y roles

--- app/api/db/clinica.tables.sql
+++ app/api/db/clinica.tables.sql
@@ -1,10 +1,31 @@
 -- Tabla Departamento
 CREATE TABLE departamento (
-    id_departamento INT PRIMARY KEY AUTO_INCREMENT,
+    id_departamento INT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     CONSTRAINT uk_departamento_nombre UNIQUE (nombre)
 );
-insert into departamento (nombre) values ('Laboratorios'), ('Cirugia'), ('Recursos Humanos'), ('Farmacia'), ('Direccion');
+insert into departamento (id_departamento, nombre) values
+(1,'Direccion'),
+(2,'Recursos Humanos'),
+(3,'Inventario'),
+(4,'Medicina General'),
+(5,'Laboratorios'),
+(6'Cirugia'),
+(7,'Enfermeria'),
+(8,'Cardiologia'),
+(9,'Neurologia'),
+(10,'Ginecologia'),
+(11,'Pediatra'),
+(12,'Oftalmologia'),
+(13,'Ortopedia'),
+(14,'Urologia'),
+(15,'Traumatologia'),
+(16,'Rehabilitacion'),
+(17,'Nutricion'),
+(18,'Psicologia'),
+(19,'Fisioterapia'),
+(20,'Odontologia'),
+(21,'Medicina Interna');
 
 -- Tabla Usuario
 CREATE TABLE usuario (
@@ -13,10 +34,12 @@ CREATE TABLE usuario (
     contra VARCHAR(255) NOT NULL,
     CONSTRAINT uk_usuario_username UNIQUE (username)
 );
-insert into usuario (username,contra) values ('superad','1');
+
+insert into usuario (username,contra) values ('admin','1');
+
 -- Tabla Permisos
 CREATE TABLE permisos (
-    id_permiso INT PRIMARY KEY AUTO_INCREMENT,
+    id_permiso INT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL UNIQUE,
     descripcion TEXT,
     departamento INT NOT NULL,
@@ -26,7 +49,7 @@ CREATE TABLE permisos (
 
 -- Tabla Rol
 CREATE TABLE rol (
-    id_rol INT PRIMARY KEY AUTO_INCREMENT,
+    id_rol INT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL,
     tipo ENUM('ADMINISTRADOR', 'NORMAL') NOT NULL,
     departamento INT NOT NULL,
@@ -36,9 +59,207 @@ CREATE TABLE rol (
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 );
-insert into rol (nombre, tipo, departamento) values ('jefe', 'ADMINISTRADOR', 1);
+-- para el departamento de direccion
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Director', 'ADMINISTRADOR', 1),
+('Sub Director', 'ADMINISTRADOR', 1), -- tiene todos los permisos
+('Secretario', 'NORMAL', 1),
+('Asistente', 'NORMAL', 1);
 
-insert into rol (nombre, tipo, departamento) values ('doctor', 'NORMAL', 1);
+--para el departamento de recursos humanos
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 2),
+('Especialista en Nóminas', 'NORMAL', 2),
+('Atencion a Empleados', 'NORMAL', 2),
+('Reclutador', 'NORMAL', 2);
+('Asistente', 'NORMAL', 2);
+
+-- para el departamento de inventario que tiene los siguientes roles Encargado(a) de Almacén,Coordinador(a) de Abastecimiento y Técnico(a) de Logística
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Encargado(a) de Almacén', 'ADMINISTRADOR', 3),
+('Coordinador(a) de Abastecimiento', 'NORMAL', 3),
+('Técnico(a) de Logística', 'NORMAL', 3);
+
+----para el departamento de medicina general que tiene los siguientes roles Médico(a) General, Recepcionista de Consultorio y Asistente Médico
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 4),
+('Médico(a) General', 'ADMINISTRADOR', 4),
+('Recepcionista de Consultorio', 'NORMAL', 4),
+('Asistente Médico', 'NORMAL', 4),
+('Enfermero(a)', 'NORMAL', 4),
+('Auxiliar de Enfermeria', 'NORMAL', 4),
+('Encargado de Inventario', 'NORMAL', 4);
+
+--para el departamento de laboratorios que tiene los siguientes roles Jefe de Laboratorio, Técnico(a) de Laboratorio y Asistente de Laboratorio
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Laboratorio', 'ADMINISTRADOR', 5),
+('Técnico(a) de Laboratorio', 'NORMAL', 5),
+('Asistente de Laboratorio', 'NORMAL', 5),
+('Encargado de Inventario', 'NORMAL', 5);
+
+--para el departamento de cirugia que tiene los siguientes roles Jefe de Departamento, Cirujano(a), anestesiologo, tecnico de cirugia y asistente de cirugia, recepcionista de cirugia
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 6),
+('Cirujano(a)', 'ADMINISTRADOR', 6),
+('Anestesiologo', 'NORMAL', 6),
+('Técnico de Cirugía', 'NORMAL', 6),
+('Recepcionista de Cirugía', 'NORMAL', 6),
+('Enfermero(a)', 'NORMAL', 6),
+('Auxiliar de Enfermeria', 'NORMAL', 6),
+('Encargado de Inventario', 'NORMAL', 6);
+
+--para el departamento de enfermeria que tiene los siguientes roles Jefe de Enfermeria, Enfermero(a) General, y otros roles son mas administrativos
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Director(a) de Enfermería', 'ADMINISTRADOR', 7),
+('Subdirector(a) de Enfermería', 'ADMINISTRADOR', 7),
+('Asistente Administrativo de Enfermería', 'NORMAL', 7),
+('Recepcionista de Enfermeria', 'NORMAL', 7);
+
+--para el departamento de cardiologia que tiene los siguientes roles Jefe de Departamento, Cardiologo(a), Tecnico de Cardiologia, Recepcionista de Cardiologia
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 8),
+('Cardiologo(a)', 'ADMINISTRADOR', 8),
+('Técnico de Cardiología', 'NORMAL', 8),
+('Recepcionista de Cardiología', 'NORMAL', 8),
+('Enfermero(a)', 'NORMAL', 8),
+('Auxiliar de Enfermeria', 'NORMAL', 8),
+('Encargado de Inventario', 'NORMAL', 8);
+
+--para el departamento de neurologia que tiene los siguientes roles Jefe de Departamento, Neurologo(a), Tecnico de Neurologia, Recepcionista de Neurologia
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 9),
+('Neurologo(a)', 'ADMINISTRADOR', 9),
+('Técnico de Neurología', 'NORMAL', 9),
+('Recepcionista de Neurología', 'NORMAL', 9),
+('Enfermero(a)', 'NORMAL', 9),
+('Auxiliar de Enfermeria', 'NORMAL', 9),
+('Encargado de Inventario', 'NORMAL', 9);
+
+--para el departamento de ginecologia que tiene los siguientes roles Jefe de Departamento, Ginecologo(a), Obstetra(medico), Tecnico de Ginecologia, Recepcionista de Ginecologia
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 10),
+('Ginecologo(a)', 'ADMINISTRADOR', 10),
+('Técnico de Ginecología', 'NORMAL', 10),
+('Recepcionista de Ginecología', 'NORMAL', 10),
+('Enfermero(a)', 'NORMAL', 10),
+('Auxiliar de Enfermeria', 'NORMAL', 10),
+('Encargado de Inventario', 'NORMAL', 10);
+
+--para el departamento de pediatra que tiene los siguientes roles Jefe de Departamento, Pediatra, Tecnico de Pediatra, Recepcionista de Pediatra
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 11),
+('Pediatra', 'ADMINISTRADOR', 11),
+('Técnico de Pediatría', 'NORMAL', 11),
+('Recepcionista de Pediatría', 'NORMAL', 11),
+('Enfermero(a)', 'NORMAL', 11),
+('Auxiliar de Enfermeria', 'NORMAL', 11),
+('Encargado de Inventario', 'NORMAL', 11);
+
+--para el departamento de oftalmologia que tiene los siguientes roles Jefe de Departamento, Oftalmologo(a), Tecnico de Oftalmologia, Recepcionista de Oftalmologia
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 12),
+('Oftalmologo(a)', 'ADMINISTRADOR', 12),
+('Técnico de Oftalmología', 'NORMAL', 12),
+('Recepcionista de Oftalmología', 'NORMAL', 12),
+('Enfermero(a)', 'NORMAL', 12),
+('Auxiliar de Enfermeria', 'NORMAL', 12),
+('Encargado de Inventario', 'NORMAL', 12);
+
+--para el departamento de ortopedia que tiene los siguientes roles Jefe de Departamento, Ortopedista, Tecnico de Ortopedia, Recepcionista de Ortopedia
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 13),
+('Ortopedista', 'ADMINISTRADOR', 13),
+('Técnico de Ortopedia', 'NORMAL', 13),
+('Recepcionista de Ortopedia', 'NORMAL', 13),
+('Enfermero(a)', 'NORMAL', 13),
+('Auxiliar de Enfermeria', 'NORMAL', 13),
+('Encargado de Inventario', 'NORMAL', 13);
+
+--para el departamento de urologia que tiene los siguientes roles Jefe de Departamento, Urologo(a), Tecnico de Urologia, Recepcionista de Urologia
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 14),
+('Urologo(a)', 'ADMINISTRADOR', 14),
+('Técnico de Urología', 'NORMAL', 14),
+('Recepcionista de Urología', 'NORMAL', 14),
+('Enfermero(a)', 'NORMAL', 14),
+('Auxiliar de Enfermeria', 'NORMAL', 14),
+('Encargado de Inventario', 'NORMAL', 14);
+
+--para el departamento de traumatologia que tiene los siguientes roles Jefe de Departamento, Traumatologo(a), Tecnico de Traumatologia, Recepcionista de Traumatologia
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 15),
+('Traumatologo(a)', 'ADMINISTRADOR', 15),
+('Técnico de Traumatología', 'NORMAL', 15),
+('Recepcionista de Traumatología', 'NORMAL', 15),
+('Enfermero(a)', 'NORMAL', 15),
+('Auxiliar de Enfermeria', 'NORMAL', 15),
+('Encargado de Inventario', 'NORMAL', 15);
+
+--para el departamento de rehabilitacion que tiene los siguientes roles Jefe de Departamento, Fisioterapeuta, Terapeuta Ocupacional, Recepcionista de Rehabilitacion
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 16),
+('Fisioterapeuta', 'ADMINISTRADOR', 16),
+('Terapeuta Ocupacional', 'NORMAL', 16),
+('Recepcionista de Rehabilitación', 'NORMAL', 16),
+('Enfermero(a)', 'NORMAL', 16),
+('Auxiliar de Enfermeria', 'NORMAL', 16),
+('Encargado de Inventario', 'NORMAL', 16);
+
+--para el departamento de nutricion que tiene los siguientes roles Jefe de Departamento, Nutriologo(a), Tecnico de Nutricion, Recepcionista de Nutricion
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 17),
+('Nutriologo(a)', 'ADMINISTRADOR', 17),
+('Técnico de Nutrición', 'NORMAL', 17),
+('Recepcionista de Nutrición', 'NORMAL', 17),
+('Enfermero(a)', 'NORMAL', 17),
+('Auxiliar de Enfermeria', 'NORMAL', 17),
+('Encargado de Inventario', 'NORMAL', 17);
+
+
+--para el departamento de psicologia que tiene los siguientes roles Jefe de Departamento, Psicologo(a), Tecnico de Psicologia, Recepcionista de Psicologia, encargado de inventario, enfermero(a), auxiliar de enfermeria
+
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 18),
+('Psicologo(a)', 'ADMINISTRADOR', 18),
+('Técnico de Psicología', 'NORMAL', 18),
+('Recepcionista de Psicología', 'NORMAL', 18),
+('Enfermero(a)', 'NORMAL', 18),
+('Auxiliar de Enfermeria', 'NORMAL', 18),
+('Encargado de Inventario', 'NORMAL', 18);
+
+--para el departamento de fisioterapia que tiene los siguientes roles Jefe de Departamento, Fisioterapeuta, Tecnico de Fisioterapia, Recepcionista de Fisioterapia, encargado de inventario, enfermero(a), auxiliar de enfermeria
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 19),
+('Fisioterapeuta', 'ADMINISTRADOR', 19),
+('Técnico de Fisioterapia', 'NORMAL', 19),
+('Recepcionista de Fisioterapia', 'NORMAL', 19),
+('Enfermero(a)', 'NORMAL', 19),
+('Auxiliar de Enfermeria', 'NORMAL', 19),
+('Encargado de Inventario', 'NORMAL', 19);
+
+
+--para el departamento de odontologia que tiene los siguientes roles Jefe de Departamento, Odontologo(a), Tecnico de Odontologia, Recepcionista de Odontologia, encargado de inventario, enfermero(a), auxiliar de enfermeria
+
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 20),
+('Odontologo(a)', 'ADMINISTRADOR', 20),
+('Técnico de Odontología', 'NORMAL', 20),
+('Recepcionista de Odontología', 'NORMAL', 20),
+('Enfermero(a)', 'NORMAL', 20),
+('Auxiliar de Enfermeria', 'NORMAL', 20),
+('Encargado de Inventario', 'NORMAL', 20);
+
+--para el departamento de medicina interna que tiene los siguientes roles Jefe de Departamento, Medico Internista, Tecnico de Medicina Interna, Recepcionista de Medicina Interna, encargado de inventario, enfermero(a), auxiliar de enfermeria
+insert into rol (id_rol, nombre, tipo, departamento) values
+('Jefe de Departamento', 'ADMINISTRADOR', 21),
+('Medico Internista', 'ADMINISTRADOR', 21),
+('Técnico de Medicina Interna', 'NORMAL', 21),
+('Recepcionista de Medicina Interna', 'NORMAL', 21),
+('Enfermero(a)', 'NORMAL', 21),
+('Auxiliar de Enfermeria', 'NORMAL', 21),
+('Encargado de Inventario', 'NORMAL', 21);
+
+
 insert into permisos (nombre, descripcion, departamento) values ('VP1', 'Ver listado de pacientes', 1);
 
 
